0381f5f1ab44fa318d550cb62ce08815beb9ccb1
Dan Tech <danhtran.dev@outlook.com>
2024-03-07 12:11:48 +0700

    Hello GET

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-const greeting = 'Hello RENEC!'
-console.log(greeting)
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello RENEC!')
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running at http://localhost:${process.env.PORT}`)
+})
